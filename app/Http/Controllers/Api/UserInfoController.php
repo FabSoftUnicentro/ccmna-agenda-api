4ed7057691f3fefe40e1071db100ef15b327af90
Gianluca Bine <gian_bine@hotmail.com>
2022-08-16 21:05:25 -0300

    fix: refatoração de código

--- app/Http/Controllers/Api/UserInfoController.php
+++ app/Http/Controllers/Api/UserInfoController.php
@@ -4,14 +4,21 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Api;
 
+use App\Enums\Version;
 use App\Http\Resources\v1_0\UserResource;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
 
 class UserInfoController extends Controller
 {
-    public function __invoke(Request $request): UserResource
+    public function __invoke(Request $request, Version $version): UserResource
     {
+        abort_unless(
+            $version->greaterThanOrEqualsTo(Version::v1_0),
+            Response::HTTP_NOT_FOUND
+        );
+
         return UserResource::make($request->user());
     }
 }
